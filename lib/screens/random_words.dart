f6b00d677991b47209d0f174d60e79fa8ba1f6d6
iamzer <iamzer@hotmail.com>
2019-09-04 15:25:45 +0700

    ok

--- lib/screens/random_words.dart
+++ lib/screens/random_words.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
+
 class RandomWord extends StatefulWidget {
   @override
   _RandomWordState createState() => _RandomWordState();
 }
 
 class _RandomWordState extends State<RandomWord> {
+
+  final randomWords = new WordPair.random();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('Random Words'),),
+      body: ListView(
+        children: <Widget>[
+
+        ],
+      ),
+    );
   }
 }
