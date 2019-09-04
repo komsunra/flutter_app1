2fb0856d977c7afde58a9358142f738ada73e5fc
iamzer <iamzer@hotmail.com>
2019-09-04 15:15:36 +0700

    ok

---
+++ lib/screens/random_words.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RandomWord extends StatefulWidget {
+  @override
+  _RandomWordState createState() => _RandomWordState();
+}
+
+class _RandomWordState extends State<RandomWord> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
