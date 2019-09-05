ee0ac8d1f554886a5115205139f2b680eb3fd98c
iamzer <iamzer@hotmail.com>
2019-09-05 08:29:02 +0700

    ok

--- lib/screens/random_words.dart
+++ lib/screens/random_words.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:english_words/english_words.dart';
 
 class RandomWord extends StatefulWidget {
   @override
@@ -9,16 +9,30 @@ class RandomWord extends StatefulWidget {
 class _RandomWordState extends State<RandomWord> {
 
   final randomWords = new WordPair.random();
+  final randomWordArray = <WordPair>[];
+  Widget buildListTile(WordPair wordPair){
+    return ListTile(
+      title: Text(wordPair.asUpperCase),
+      subtitle: Text('Sub title'),
+      leading: Icon(Icons.person),
+      trailing: Icon(Icons.keyboard_arrow_right),
+      isThreeLine: true,
+
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Random Words'),),
-      body: ListView(
-        children: <Widget>[
-
-        ],
-      ),
+      body: ListView.builder(itemBuilder: (context,index){
+        if(index >= randomWordArray.length){
+          randomWordArray.addAll(generateWordPairs().take(10));
+        }
+        return buildListTile(randomWordArray[index]);
+      },
+      itemCount: 20,)
     );
   }
 }
